78cb08b68097a478f37be31a17eae65f819fc1d1
fachiri <fachrycooles@gmail.com>
2024-06-07 21:50:27 +0800

    refactor: remove comments

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Instal dependensi menggunakan Composer
 composer install
-
-# Optimalkan aplikasi Laravel
 php artisan optimize
-
-# Instal dependensi JavaScript menggunakan npm
 npm install
-
-# Build aplikasi frontend (jika diperlukan)
 npm run build
