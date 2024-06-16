f7cee648e5be687f4b7cf2eb347101172fbd0e79
Muh. Fachry J.K. Luid <fachrycooles@gmail.com>
2024-06-16 17:01:19 +0800

    fix: ERR_ADDRESS_INVALID vite resource

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        hmr: {
+            host: 'localhost'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
